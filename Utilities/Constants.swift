8565eabb2ae03cc686098dd7f2b45a27af9fb6e2
Kushal Pandya <kushalspandya@gmail.com>
2025-11-28 12:11:06 +0530

    Update Build Version

--- Utilities/Constants.swift
+++ Utilities/Constants.swift
@@ -91,9 +91,8 @@ enum About {
     static let appWiki = "https://github.com/kushalpandya/Petrichor/wiki"
     static let appAcknowledgements = "https://github.com/kushalpandya/Petrichor/blob/main/ACKNOWLEDGEMENTS.md"
     static let sponsor = "https://github.com/sponsors/kushalpandya"
-    static let appPlaybackQueueLabel = "org.Petrichor.playback"
-    static let appVersion = "1.0.1"
-    static let appBuild = "101"
+    static let appVersion = "1.2.3"
+    static let appBuild = "123"
 }
 
 // MARK: - Audio File Formats
